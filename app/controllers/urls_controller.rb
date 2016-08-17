fb313eb67cf21a3dd3a35dc3c395455068a39484
Ashley <ashleymsutton@outlook.com>
2016-08-17 12:04:28 -0400

    added params for url short

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,9 +1,11 @@
 class UrlsController < ApplicationController
   def new
-    @url = Url.create(name: params[:term], short: (0...5).map { ('a'..'z').to_a[rand(26)] }.join)
+    @url = Url.new
   end
 
   def create
+    @url = Url.create(name: params[:term], short: (0...5).map { ('a'..'z').to_a[rand(26)] }.join)
+    redirect_to urls_path
   end
 
   def index
@@ -14,8 +16,20 @@ class UrlsController < ApplicationController
   end
 
   def show
+    if params[:id].to_i != 0
+      @url = Url.find(params[:id])
+      redirect_to "http://#{@url.name}"
+    else
+      @url = Url.find_by(short: params[:id])
+      redirect_to "http://#{@url.name}"
+    end
+
+    # @url = Url.find_by(name: "http://#{params[:id]}")
+    # redirect_to "http://#{@url.name}"
   end
 
   def destroy
   end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ resources :urls
 
   root 'urls#new'
 
-  post 'urls/new'
+  post 'urls/create'
 
+  get '/:id' => 'urls#show'
+
+  get '/:short' => 'urls#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
